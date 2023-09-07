450b07f8e1c5f75eceb8afeb75d6d569622f31a4
Merlin Webster <merlin.webster@multiverse.io>
2023-09-07 09:26:11 +0100

    Fix package files list to remove nonexistent

--- mix.exs
+++ mix.exs
@@ -30,8 +30,7 @@ defmodule Patchwork.MixProject do
   defp package do
     [
       # These are the default files included in the package
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                  license* CHANGELOG* changelog* src),
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/mjftw/patchwork"}
     ]
