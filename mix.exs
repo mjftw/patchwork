2356f9ab0207ae009605e01450825ffca34383a3
Merlin Webster <merlin.webster@multiverse.io>
2023-09-07 09:22:22 +0100

    Added Mix package config

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Patchwork.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -15,6 +17,20 @@ defmodule Patchwork.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp description() do
+    "Patchwork is a library for calculating the difference between two data structures, and applying that difference as a patch. Think git patches for for Elixir data structures."
+  end
+
+  defp package do
+    [
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                  license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/mjftw/patchwork"}
     ]
   end
 
